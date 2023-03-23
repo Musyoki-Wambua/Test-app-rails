8c5ea1c5030b8bc8e51dea20053f56ef4cacfb8a
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-24 01:29:36 +0300

    Encoding and Decoding with JWTs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+
+    skip_before_action :authorized, only: [:create]
+
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+          render json: { data: @user }, status: :created
+        else
+          render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+      
+      def user_params
+        params.require(:user).permit(:username, :password, :bio, :avatar)
+      end
 end
