d4e810d4c9d6d4f80a7362ef6049623c9f97fb9d
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-24 01:31:18 +0300

    Implementing Login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-
     skip_before_action :authorized, only: [:create]
-
-
+  
+    def profile
+      render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+  
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { data: @user }, status: :created
-        else
-          render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
-        end
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token({ user_id: @user.id })
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :unprocessable_entity
       end
-    
-      private
-      
-      def user_params
-        params.require(:user).permit(:username, :password, :bio, :avatar)
-      end
-end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password, :bio, :avatar)
+    end
+  end
